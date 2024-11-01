d7d563889675926250122efe6c817985bb05b9a8
maxtortecnoreparaciones <maxtortecnoreparaciones@gmail.com>
2024-11-01 10:42:09 -0500

    liena de eliminacionde imagenes

--- compresorima.py
+++ compresorima.py
@@ -2,12 +2,14 @@ from PIL import Image
 
 import os
 
-downloadsFolder = "C:\\Users\\Administrador\\Downloads"
-
+downloadsFolder = "C:\\Users\\Administrador\\Downloads\\"
+ # Verifica si el archivo tiene una de las extensiones permitidas
 if __name__ == "__main__":
    for filename in os.listdir(downloadsFolder):
     name,extension = os.path.splitext(downloadsFolder + filename)
-
-    if extension in [".jpg", ".jpg", ".png"]:
+    # Guarda la imagen comprimida
+    if extension in [".jpg", ".jpeg", ".png"]:
         picture = Image.open(downloadsFolder + filename)
         picture.save(downloadsFolder + "compressed_"+filename, optimize=True, quality=60)
+        #borrar las originales que se comprimieron
+        os.remove(os.path.join(downloadsFolder, filename))
